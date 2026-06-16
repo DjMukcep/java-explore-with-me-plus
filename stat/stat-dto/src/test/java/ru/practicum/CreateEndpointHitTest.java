f01d49499a25b5072ba8498b9cc30e7ff648c27a
AlexandrNovizov <126908706+AlexandrNovizov@users.noreply.github.com>
2026-06-16 18:30:28 +0400

    fix: Исправлено название тестового класса

--- stat/stat-dto/src/test/java/ru/practicum/CreateEndpointHitTest.java
+++ stat/stat-dto/src/test/java/ru/practicum/CreateEndpointHitTest.java
@@ -14,7 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.containsString;
 
-class CreateCreateEndpointHitTest {
+class CreateEndpointHitTest {
 
     private static Validator validator;
     private CreateEndpointHit createEndpointHit;
