dc3b84f6810aa0b66404efcb2bbe6e65a0c7a400
AlexandrNovizov <126908706+AlexandrNovizov@users.noreply.github.com>
2026-06-16 18:11:48 +0400

    feat: Добавлен контроллер StatController

---
+++ stat/stat-server/src/main/java/ru/practicum/StatController.java
@@ -0,0 +1,35 @@
+package ru.practicum;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class StatController {
+
+    // TODO: Implement service
+
+    private final static String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
+    public EndpointHit hit(@RequestBody @Valid CreateEndpointHit createEndpointHit) {
+        return null;
+    }
+
+    @GetMapping("/stats")
+    public List<ViewStats> stats(
+            @RequestParam @DateTimeFormat(pattern = DATE_TIME_PATTERN) LocalDateTime start,
+            @RequestParam @DateTimeFormat(pattern = DATE_TIME_PATTERN) LocalDateTime end,
+            @RequestParam(required = false) List<String> uris,
+            @RequestParam(required = false, defaultValue = "false") boolean unique) {
+
+        return List.of();
+    }
+}
