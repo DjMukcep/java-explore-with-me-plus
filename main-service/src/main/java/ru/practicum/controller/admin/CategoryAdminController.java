36d288e56d0dea20d922834f3d568dc9a27ce650
Iskender Mamedov <i.mamedov@hh.ru>
2026-06-24 00:05:13 +0700

    some fixes

--- main-service/src/main/java/ru/practicum/controller/admin/CategoryAdminController.java
+++ main-service/src/main/java/ru/practicum/controller/admin/CategoryAdminController.java
@@ -16,7 +16,7 @@ import ru.practicum.entity.category.CategoryService;
  */
 
 @RestController
-@RequestMapping(("/admin/categories"))
+@RequestMapping(path = "/admin/categories")
 @RequiredArgsConstructor
 @Slf4j
 public class CategoryAdminController {
@@ -26,18 +26,25 @@ public class CategoryAdminController {
     @ResponseStatus(HttpStatus.CREATED)
     public CategoryDto create(@Valid @RequestBody NewCategoryDto payload) {
         log.info("Handling category create: {}", payload);
-        return categoryService.create(payload);
+        CategoryDto response = categoryService.create(payload);
+        log.info("Created category: {}", response);
+        return response;
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable long id) {
+        log.info("Handling category delete with ID = {}", id);
         categoryService.delete(id);
+        log.info("Deleted category with ID = {}", id);
     }
 
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public CategoryDto update(@PathVariable long id, @Valid @RequestBody CategoryDto payload) {
-        return categoryService.update(id, payload);
+        log.info("Handling category update: ID = {}, payload = {}", id, payload);
+        CategoryDto response = categoryService.update(id, payload);
+        log.info("Updated category with ID = {}, payload = {}", id, payload);
+        return response;
     }
 }
--- main-service/src/main/java/ru/practicum/entity/category/Category.java
+++ main-service/src/main/java/ru/practicum/entity/category/Category.java
@@ -14,6 +14,18 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(unique = true, nullable = false)
     private String name;
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        return id != null && id.equals(((Category) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
