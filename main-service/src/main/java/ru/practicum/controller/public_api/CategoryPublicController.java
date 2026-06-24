c2ae1195a91c47441f5652902747100b8887cb21
Iskender Mamedov <i.mamedov@hh.ru>
2026-06-24 11:24:43 +0700

    added @ResponseStatus'es

--- main-service/src/main/java/ru/practicum/controller/public_api/CategoryPublicController.java
+++ main-service/src/main/java/ru/practicum/controller/public_api/CategoryPublicController.java
@@ -3,6 +3,7 @@ package ru.practicum.controller.public_api;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.category.CategoriesParamDto;
 import ru.practicum.dto.category.CategoryDto;
@@ -24,11 +25,13 @@ public class CategoryPublicController {
     private final CategoryService categoryService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<CategoryDto> findAll(@ModelAttribute @Valid CategoriesParamDto params) {
         return categoryService.findAll(params);
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public CategoryDto findById(@PathVariable long id) {
         return categoryService.findById(id);
     }
