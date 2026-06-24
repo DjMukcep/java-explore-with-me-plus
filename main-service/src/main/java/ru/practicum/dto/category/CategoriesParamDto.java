372b2ca4624cb8e66fe9b6c89b089ed2e6e4941b
Iskender Mamedov <i.mamedov@hh.ru>
2026-06-24 13:48:20 +0700

    added tests

--- main-service/src/main/java/ru/practicum/dto/category/CategoriesParamDto.java
+++ main-service/src/main/java/ru/practicum/dto/category/CategoriesParamDto.java
@@ -2,13 +2,15 @@ package ru.practicum.dto.category;
 
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CategoriesParamDto {
     @PositiveOrZero(message = "can't be negative")
-    private int from = 0;
+    private int from;
 
     @Positive(message = "must be greater than 0")
-    private int size = 10;
+    private int size;
 }
