4c88f5b213f6dd4588dc130975047f4ed81befbc
Iskender Mamedov <i.mamedov@hh.ru>
2026-06-24 15:02:15 +0700

    added defaults for CategoriesParamDto

--- main-service/src/main/java/ru/practicum/dto/category/CategoriesParamDto.java
+++ main-service/src/main/java/ru/practicum/dto/category/CategoriesParamDto.java
@@ -2,15 +2,21 @@ package ru.practicum.dto.category;
 
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CategoriesParamDto {
     @PositiveOrZero(message = "can't be negative")
-    private int from;
+    @Builder.Default
+    private int from = 0;
 
     @Positive(message = "must be greater than 0")
-    private int size;
+    @Builder.Default
+    private int size = 10;
 }
