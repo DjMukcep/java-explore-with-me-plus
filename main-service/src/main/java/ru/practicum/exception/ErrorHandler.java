7c9b80e185353b9f7cd76d0b31640960a996d045
Roman <romg7755@gmail.com>
2026-06-25 18:36:37 +0300

    fix: ErrorHandler добавить необходимые аннотации, обработка ConstraintViolationException

--- main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -20,13 +21,25 @@ public class ErrorHandler {
     private static final DateTimeFormatter formatter =
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleConstraintViolation(final ConstraintViolationException e) {
+        log.warn("Ошибка валидации : {}",  e.getMessage());
+
+        return ApiError.builder()
+                .message(e.getMessage())
+                .reason("Incorrectly made request.")
+                .status(HttpStatus.BAD_REQUEST.name())
+                .timestamp(LocalDateTime.now().format(formatter))
+                .build();
+    }
+
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleNotFoundException(final NotFoundException e) {
         log.warn("Сущность не найдена: {}",e.getMessage());
 
         return ApiError.builder()
-                .errors(null)
                 .message(e.getMessage())
                 .reason("Not Found!")
                 .status(HttpStatus.NOT_FOUND.name())
@@ -61,7 +74,6 @@ public class ErrorHandler {
         log.warn("Ошибка валидации: {}",e.getMessage());
 
         return ApiError.builder()
-                .errors(null)
                 .message(e.getMessage())
                 .reason("Incorrectly made request.")
                 .status(HttpStatus.BAD_REQUEST.name())
@@ -75,7 +87,6 @@ public class ErrorHandler {
         log.warn("Нарушение целостности данных: {}",e.getMessage());
 
         return ApiError.builder()
-                .errors(null)
                 .message(e.getMessage())
                 .reason("Integrity constraint has been violated.")
                 .status(HttpStatus.CONFLICT.name())
@@ -87,7 +98,6 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ApiError handleConditionsNotMetException(final ConditionsNotMetException e) {
         return ApiError.builder()
-                .errors(null)
                 .message(e.getMessage())
                 .reason("Logical conditions hasn't met.")
                 .status(HttpStatus.CONFLICT.name())
@@ -95,11 +105,12 @@ public class ErrorHandler {
                 .build();
     }
 
+    @ExceptionHandler(ForbiddenException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     public ApiError handleForbiddenException(final ForbiddenException e) {
         log.warn("Нет прав доступа: {}",e.getMessage());
 
         return ApiError.builder()
-                .errors(null)
                 .message(e.getMessage())
                 .reason("For the requested operation the conditions are not met.")
                 .status(HttpStatus.FORBIDDEN.name())
@@ -113,7 +124,6 @@ public class ErrorHandler {
         log.error("Внутренняя ошибка main-service: {} ", e.getMessage(), e);
 
         return ApiError.builder()
-                .errors(null)
                 .message("Internal Server Error")
                 .reason("Error occurred on the server side.")
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.name())
